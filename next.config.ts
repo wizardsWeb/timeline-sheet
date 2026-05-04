472c4c8b3b993ae15e546cf77863fbe331f2cb41
wizardsWeb <voravinay82@gmail.com>
2026-05-04 12:34:59 +0530

    final push

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.ignoreWarnings = [
+      ...(config.ignoreWarnings || []),
+      { module: /node_modules\/jose/ },
+    ];
+    return config;
+  },
 };
 
 export default nextConfig;
